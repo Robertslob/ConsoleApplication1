3cac12b3e64dc63a6fdbbd234552034b2c573bb2
Robertslob <rjrslob@gmail.com>
2016-11-30 16:09:53 +0100

    Zoekmodus werkt, alleen nog eigen lock implementeren

--- ConsoleApplication1/Checker.cs
+++ ConsoleApplication1/Checker.cs
@@ -13,9 +13,9 @@ namespace ConsoleApplication1
         private static bool tel;
         private static Object LOCK = new Object();
 
-        public Checker(int lower, int upper, int m, int p, int u)
+        public Checker(int lower, int upper, int m, int p, int u, string s)
         {
-            if (u == 0)
+            if (u == 0 | u == 2)
                 tel = true;
             else
             {
@@ -28,8 +28,12 @@ namespace ConsoleApplication1
             {
                 int from = lower + (int)((i * (long)(upper-lower)) / p);
                 int to = lower + (int)(((i +1)* (long)(upper-lower)) / p);
-                if(tel)
-                    ts[i] = new Thread(() => CheckBoundariesTel(from, to, m));
+                if (tel)
+                {
+                    if (u == 0)
+                        ts[i] = new Thread(() => CheckBoundariesTel(from, to, m));
+                    else ts[i] = new Thread(() => CheckBoundariesHash(from, to, m, s));
+                }
                 else ts[i] = new Thread(() => CheckBoundariesList(from, to, m));
             }
             
@@ -42,8 +46,10 @@ namespace ConsoleApplication1
             {
                 ts[i].Join();
             }
-            if(tel)
+            if(tel && u ==0)
                 Console.WriteLine(totalAllThreads);
+            if (totalAllThreads == 0 && u == 2)
+                Console.WriteLine("-1");
             
         }
 
@@ -86,6 +92,32 @@ namespace ConsoleApplication1
                     total++;
             }
             Interlocked.Add(ref totalAllThreads, total);
+        }
+
+        private static void CheckBoundariesHash(int low, int upper, int m, string hash)
+        {
+            SHA1Managed sha = new SHA1Managed();
+            int k = 274856182;
+            //Console.WriteLine(BitConverter.ToString((sha.ComputeHash(Encoding.UTF8.GetBytes(k.ToString())))));
+            for (int i = low; i < upper; i++)
+            {
+                if (totalAllThreads == 0)
+                {
+                    int sum = sumNumber(i);
+                    if (sum % m == 0)
+                    {
+                        // Credits for converting the int to a hash in the correct form go to Mitch
+                        // http://stackoverflow.com/questions/17292366/hashing-with-sha1-algorithm-in-c-sharp
+                        byte[] shaHash = sha.ComputeHash(Encoding.UTF8.GetBytes(i.ToString()));
+                        if (string.Join("", shaHash.Select(b => b.ToString("x2")).ToArray()) == hash)
+                        {
+                            Console.WriteLine(i);
+                            Interlocked.Increment(ref totalAllThreads);
+                        }
+                    }                        
+                }
+                else break;
+            }
         }
     }
 }
--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -11,15 +11,15 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             string s = Console.ReadLine();
-            string[] numbers = s.Split(' ');
-            string hash = Console.ReadLine();
+            string[] numbers = s.Split(' ');            
             int l = int.Parse(numbers[0]);
             int b = int.Parse(numbers[1]);
             int e = int.Parse(numbers[2]);
             int m = int.Parse(numbers[3]);
             int p = int.Parse(numbers[4]);
             int u = int.Parse(numbers[5]);
-            Checker c = new Checker(b, e, m, p, u);
+            string hash = numbers[6];
+            Checker c = new Checker(b, e, m, p, u, hash);
             Console.ReadLine();
         }
     }
